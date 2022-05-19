d5a35cc7d9d7a06bfc1c229708bd82866d7c4a61
Steve Billings <billings@synopsys.com>
2022-05-19 16:42:48 -0400

    fix: fix resource leak found by coverity

--- src/main/java/com/synopsys/integration/blackduck/dockerinspector/config/DockerInspectorSystemProperties.java
+++ src/main/java/com/synopsys/integration/blackduck/dockerinspector/config/DockerInspectorSystemProperties.java
@@ -7,40 +7,40 @@
  */
 package com.synopsys.integration.blackduck.dockerinspector.config;
 
-import com.synopsys.integration.exception.IntegrationException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import com.synopsys.integration.exception.IntegrationException;
+
 @Component
 public class DockerInspectorSystemProperties {
-  private  final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-  public void augmentSystemProperties(final String additionalSystemPropertiesPath) throws IntegrationException {
-    if (StringUtils.isNotBlank(additionalSystemPropertiesPath)) {
-      logger.debug(String.format("Reading user-provided additional System properties: %s", additionalSystemPropertiesPath));
-      File additionalSystemPropertiesFile = new File(additionalSystemPropertiesPath);
-      Properties additionalSystemProperties = new Properties();
-      InputStream additionalSystemPropertiesInputStream = null;
-      try {
-        additionalSystemPropertiesInputStream = new FileInputStream(additionalSystemPropertiesFile);
-        additionalSystemProperties.load(additionalSystemPropertiesInputStream);
-        for (Object key : additionalSystemProperties.keySet()) {
-          String keyString = (String) key;
-          String value = additionalSystemProperties.getProperty(keyString);
-          logger.trace(String.format("additional system property: %s", keyString));
-          System.setProperty(keyString, value);
+    public void augmentSystemProperties(String additionalSystemPropertiesPath) throws IntegrationException {
+        if (StringUtils.isNotBlank(additionalSystemPropertiesPath)) {
+            logger.debug(String.format("Reading user-provided additional System properties: %s", additionalSystemPropertiesPath));
+            File additionalSystemPropertiesFile = new File(additionalSystemPropertiesPath);
+            Properties additionalSystemProperties = new Properties();
+            try (InputStream additionalSystemPropertiesInputStream = new FileInputStream(additionalSystemPropertiesFile)) {
+                additionalSystemProperties.load(additionalSystemPropertiesInputStream);
+                for (Object key : additionalSystemProperties.keySet()) {
+                    String keyString = (String) key;
+                    String value = additionalSystemProperties.getProperty(keyString);
+                    logger.trace(String.format("additional system property: %s", keyString));
+                    System.setProperty(keyString, value);
+                }
+            } catch (IOException e) {
+                String msg = String.format("Error loading additional system properties from %s: %s", additionalSystemPropertiesPath, e.getMessage());
+                throw new IntegrationException(msg);
+            }
         }
-      } catch (IOException e) {
-        final String msg = String.format("Error loading additional system properties from %s: %s", additionalSystemPropertiesPath, e.getMessage());
-        throw new IntegrationException(msg);
-      }
     }
-  }
 }
